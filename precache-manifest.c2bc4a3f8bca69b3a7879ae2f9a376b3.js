0c17619adba4b7a2ff4f904e1e471c6d15acec56
Henry_Rothenberg <hsr205@aol.com>
2020-06-09 16:38:41 -0400

    Updates

---
+++ precache-manifest.c2bc4a3f8bca69b3a7879ae2f9a376b3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8558e6645efc48b9fe49e35e0be224",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc775cf84a3d9ca0106e",
+    "url": "/static/js/2.c0f7feb2.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.c0f7feb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7262051d45777aea69ed",
+    "url": "/static/js/main.a90429b2.chunk.js"
+  },
+  {
+    "revision": "b571333912a100c89368",
+    "url": "/static/js/runtime-main.c57d8233.js"
+  },
+  {
+    "revision": "980e6191a8825d201f9c96a90986cee6",
+    "url": "/static/media/Ben_Kestenbaum.980e6191.jpg"
+  },
+  {
+    "revision": "8f3b66b2c7c4b2b47bc5f97701ca013b",
+    "url": "/static/media/BlaxBaum_Logo.8f3b66b2.png"
+  },
+  {
+    "revision": "a4757cd0f29ec8d2f85104ecb03e4b74",
+    "url": "/static/media/Gav_Blaxberg.a4757cd0.jpg"
+  },
+  {
+    "revision": "4fbdd60e190ae4a0d10eacb3798fcf08",
+    "url": "/static/media/Henry_Rothenberg.4fbdd60e.jpeg"
+  },
+  {
+    "revision": "27b0ad0b38e00f01a77d8057ca985674",
+    "url": "/static/media/logo.27b0ad0b.png"
+  }
+]);
